2179de0e07e151b03d14067a038ab1feff54c290
kurtesy <np210291@gmail.com>
2020-05-01 00:29:59 +0530

    Changed State management for canvas object Added background image demo fro canvas To JSON will now copy the json to clipboard.

--- src/DesignCanvas.js
+++ src/DesignCanvas.js
@@ -18,53 +18,71 @@ class DesignCanvas extends React.Component {
     height: 300,
   }
 
+  canvas = new fabric.Canvas(this.c);
+
   state = {
-    canvas: null,
     mousecursor: null,
     show: {rect: false,
-    circle: false,
-    image: false}
+      circle: false,
+      image: false},
+    backgroudImg: null
   }
 
   componentDidMount() {
-    const canvas = new fabric.Canvas(this.c)
-    canvas.freeDrawingBrush.width = 10;
-    canvas.isDrawingMode = true;
-    canvas.freeDrawingBrush.color = "#ff0000";
-    this.setState({ canvas })
+    this.canvas = new fabric.Canvas(this.c)
+    this.canvas.freeDrawingBrush.width = 10;
+    this.canvas.isDrawingMode = true;
+    this.canvas.freeDrawingBrush.color = "#ff0000";
   }
 
   updatedCanvas = newCanvas => {
-    if (typeof newCanvas !== 'undefined') {
-      this.setState({ canvas: newCanvas })
-      console.log('Updating canvas to', newCanvas)
-    }
+    this.canvas = newCanvas;
   }
 
   onControlChange = data => {
-    let canvas = this.state.canvas;
-    if (canvas) {
-      console.log(data);
-      canvas.freeDrawingBrush.width = data.brushSize;
-      canvas.isDrawingMode = data.canDraw;
-      canvas.freeDrawingBrush.color = data.brushcolor;
-      this.setState({canvas});
-    }
+    this.canvas.freeDrawingBrush.width = data.brushsize;
+    this.canvas.isDrawingMode = data.canDraw;
+    this.canvas.freeDrawingBrush.color = data.brushcolor;
+    this.canvas.renderAll();
+    console.log('onControlChange', this.canvas.freeDrawingBrush);
   }
 
   showFigures = data => {
     this.setState({show:data});
   }
 
-  clear = e =>
-    {
-      const show = {rect: false,
-        circle: false,
-        image: false}
-      e.preventDefault()
-      this.state.canvas.clear();
-      this.setState({show});
-    }
+  addBackgroudImg = imagePath => {
+    console.log(imagePath);
+    this.setState({backgroudImg: imagePath})
+    let canvas = this.canvas;
+    fabric.Image.fromURL(imagePath, function(img) {
+      // add background image
+      canvas.setBackgroundImage(img, canvas.renderAll.bind(canvas), {
+        scaleX: canvas.width / img.width,
+        scaleY: canvas.height / img.height
+      });
+    });
+    this.canvas = canvas;
+  }
+
+  canvasToJson = event => {
+      event.preventDefault()
+      let jsonData = JSON.stringify(this.canvas.toJSON());
+      navigator.clipboard.writeText(jsonData)
+        .then(r => {alert('JSON Copied to clipboard')});
+      console.log(jsonData);
+  }
+
+  clear = e => {
+    const show = {rect: false,
+      circle: false,
+      image: false}
+    e.preventDefault()
+    this.canvas.clear();
+    this.canvas.backgroundImage = null;
+    this.canvas.renderAll();
+    this.setState({show});
+  }
 
   canvasStyle = {
     borderStyle: "solid",
@@ -75,31 +93,27 @@ class DesignCanvas extends React.Component {
   render() {
     const children = React.Children.map(this.props.children, child => {
       return React.cloneElement(child, {
-        canvas: this.state.canvas,
+        canvas: this.canvas,
         updatedCanvas: this.updatedCanvas,
       })
     })
     const { width, height } = this.props
     return (
       <Fragment>
-        <ControlPanel 
-          canvas={this.state.canvas} 
+        <ControlPanel
+          canvas={this.canvas}
           onChange={this.onControlChange}
-          showFigures={this.showFigures}/>
-        
+          showFigures={this.showFigures}
+          addBackgroudImg={this.addBackgroudImg}/>
+
         <canvas ref={c => (this.c = c)} width={width} height={height} style={this.canvasStyle}/>
-        {this.state.canvas && children}
-        {this.state.show.rect && <Rect width={100} height={100} fill="blue" canvas={this.state.canvas}/>}
-        {this.state.show.circle && <Circle radius={20} top={200} canvas={this.state.canvas}/>}
-        {this.state.show.image && <Image url="https://http.cat/100" scale={0.2} top={100} canvas={this.state.canvas}/>}
+        {this.canvas && children}
+        {this.state.show.rect && <Rect width={100} height={100} fill="blue" canvas={this.canvas}/>}
+        {this.state.show.circle && <Circle radius={20} top={200} canvas={this.canvas}/>}
+        {this.state.show.image && <Image url="https://http.cat/100" scale={0.2} top={100} canvas={this.canvas}/>}
         <br />
         <button
-          onClick={e => {
-            e.preventDefault()
-            let jsonData = this.state.canvas.toJSON();
-            console.log(JSON.stringify(jsonData));
-          }}
-        >
+          onClick={e => this.canvasToJson(e)}>
           To JSON
         </button>
         <button
@@ -112,4 +126,4 @@ class DesignCanvas extends React.Component {
   }
 }
 
-export default DesignCanvas
+export default DesignCanvas;
