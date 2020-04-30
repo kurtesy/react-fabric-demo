4f9dc8418aab82826028cc9a42b79bb4c1db38bf
kurtesy <np210291@gmail.com>
2020-04-30 21:57:56 +0530

    Updates

---
+++ precache-manifest.3312bd338b6eb117884b46542cb8e299.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad945987cbd15c5564db691ee6b43d05",
+    "url": "/kurtesy/react-fabric-demo/tree/develop/index.html"
+  },
+  {
+    "revision": "5de2ceba9f12e12e4b11",
+    "url": "/kurtesy/react-fabric-demo/tree/develop/static/js/2.6f739b45.chunk.js"
+  },
+  {
+    "revision": "5de4ca19518a583c1ffe7d9ae4fef4e8",
+    "url": "/kurtesy/react-fabric-demo/tree/develop/static/js/2.6f739b45.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "115e011e65777673a044",
+    "url": "/kurtesy/react-fabric-demo/tree/develop/static/js/main.17b6dfdf.chunk.js"
+  },
+  {
+    "revision": "f3f086e3009cd3a712f0",
+    "url": "/kurtesy/react-fabric-demo/tree/develop/static/js/runtime-main.87dae2a1.js"
+  }
+]);
