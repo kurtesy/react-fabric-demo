d927affcb22532a7d34997af8a243d42c0af24c9
kurtesy <np210291@gmail.com>
2020-05-06 10:25:36 +0530

    New Built

---
+++ build/precache-manifest.c947e0dff896801f62873cd0bb9465bc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4b17242ee677a5dd985f02fba12b995c",
+    "url": "/react-fabric-demo/index.html"
+  },
+  {
+    "revision": "68333fe15860737e0238",
+    "url": "/react-fabric-demo/static/css/main.cbdacb85.chunk.css"
+  },
+  {
+    "revision": "ae9bb8ac797015f86f86",
+    "url": "/react-fabric-demo/static/js/2.ef61bd10.chunk.js"
+  },
+  {
+    "revision": "d5b0182359046717847cba8f139ad76b",
+    "url": "/react-fabric-demo/static/js/2.ef61bd10.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "68333fe15860737e0238",
+    "url": "/react-fabric-demo/static/js/main.b36b740d.chunk.js"
+  },
+  {
+    "revision": "7443e8714512d586e29f",
+    "url": "/react-fabric-demo/static/js/runtime-main.15bfecaf.js"
+  }
+]);
